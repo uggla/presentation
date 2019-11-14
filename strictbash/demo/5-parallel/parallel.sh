e724a5e479d8bc2705527e92ae165d1331aff5d3
Uggla <uggla@free.fr>
2019-11-14 01:20:33 +0100

    Fix parallel script

--- strictbash/demo/5-parallel/parallel.sh
+++ strictbash/demo/5-parallel/parallel.sh
@@ -2,7 +2,6 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-echo "wget 'https://www.gnu.org/graphics/babies/BabyGnu.png' \n
+printf "wget 'https://www.gnu.org/graphics/babies/BabyGnu.png' \n
       wget 'https://download.tuxfamily.org/libergnu/igzz/2014/10/gnu-fsf.png' \n
-      wget 'https://www.gnu.org/graphics/bahlon/3d-gnu-head.jpg'" |
-  parallel
+      wget 'https://www.gnu.org/graphics/bahlon/3d-gnu-head.jpg'" | parallel
